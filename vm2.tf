a1fba474fc1fc3d2013ea43e7de67bf9fc0ab686
prasantkumardas <102045062+prasantkumardas@users.noreply.github.com>
2024-09-14 01:00:47 +0530

    Update vm2.tf

--- vm2.tf
+++ vm2.tf
@@ -1,19 +1,1 @@
 
-
-resource "azurerm_resource_group" "example" {
-    name = "trainings2030"
-    location = "east us"
-}
-
-resource "azurerm_virtual_network" "example" {
-    name = "vnet1"
-    address_space = [ 10.0.0.5/16]
-    location = azurerm_resource_group.example.location
-    resouresource_group_name = azurerm_resource_group.example.name
-
-}
-resource "azurerm_virtual_machine" "example" {
-    name = mylinuxvm
-    resource_group_name = azurerm_linux_virtual_machine.example
-  
-}
